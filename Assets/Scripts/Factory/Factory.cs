fabc46f923f8e64f957c42034baaf348296ef930
Simon Binet <binetsimon@gmail.com>
2021-05-31 21:53:45 +0200

    ajout des boutons

---
+++ Assets/Scripts/Factory/Factory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+    public GameObject monster;
+    public GameObject gameManager;
+    private List<GameObject> monstersWaiting;
+
+    public int recondite;
+    public int speedRessource;
+    public int healthRessource;
+    public int seekRessource;
+    public int visionRessource;
+    public int strengthRessource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void InstantiateMonster()
+    {
+        monstersWaiting.Add((GameObject) Instantiate(monster));
+        Debug.Log(monstersWaiting.Count);
+        monstersWaiting[0].GetComponent<Monster>().movementSpeed = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnMonster()
+    {
+        InstantiateMonster();
+    }
+}
---
+++ Assets/Scripts/Factory/FactoryClickHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FactoryClickHandler : MonoBehaviour
+{
+    public KeyCode _key;
+    private Button _button;
+    void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_key))
+        {
+            FadeToColor(_button.colors.pressedColor);
+            _button.onClick.Invoke();
+        }else if (Input.GetKeyUp(_key))
+        {
+            FadeToColor(_button.colors.normalColor);
+        }
+    }
+
+    void FadeToColor(Color color)
+    {
+        Graphic graphic = GetComponent<Graphic>();
+        graphic.CrossFadeColor(color, _button.colors.fadeDuration, true, true);
+    }
+}
