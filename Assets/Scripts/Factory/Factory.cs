6af45a585889aa688e2a7c98edd81142bfcef069
Simon Binet <binetsimon@gmail.com>
2021-06-02 19:48:18 +0200

    script factory refactored

--- Assets/Scripts/Factory/Factory.cs
+++ Assets/Scripts/Factory/Factory.cs
@@ -7,28 +7,23 @@ public class Factory : MonoBehaviour
     public GameObject monster;
     public GameObject gameManager;
 
-    private int[] currentRessourceMonster;
-    private Queue<int[]> monstersWaiting;
+    private enum Ressource { RECONDITE, SODA, MEAT, WEED, FUNGUS, PURPLE_CRISTAL};
+
+    private Queue<Ressource> monsterWaiting;
 
     public int recondite;
-    public int movementSpeedRessource;
-    public int hitPointsRessource;
-    public int carryCapacityRessource;
-    public int lineOfSightRessource;
-    public int strengthRessource;
+    public int soda;
+    public int meat;
+    public int weed;
+    public int fungus;
+    public int purpleCristal;
 
     // Start is called before the first frame update
     void Start()
     {
-        monstersWaiting = new Queue<int[]>();
-        CreateRessourceMonster();
-
+        NewMonster();
     }
 
-    void CreateRessourceMonster()
-    {
-        currentRessourceMonster = new int[6] { 0, 0, 0, 0, 0, 0 };
-    }
 
     // Update is called once per frame
     void Update()
@@ -36,28 +31,14 @@ public class Factory : MonoBehaviour
         
     }
 
-    public void CancelMonster()
+    public void NewMonster()
     {
-        movementSpeedRessource += currentRessourceMonster[1];
-        hitPointsRessource += currentRessourceMonster[2];
-        carryCapacityRessource += currentRessourceMonster[3];
-        lineOfSightRessource += currentRessourceMonster[4];
-        strengthRessource += currentRessourceMonster[5];
-        CreateRessourceMonster();
+        monsterWaiting = new Queue<Ressource>();
     }
 
-    public void createMonster()
+    public void SpawnMonster()
     {
-        if (currentRessourceMonster[0]>0)
-        {
-            monstersWaiting.Enqueue(currentRessourceMonster);
-            CreateRessourceMonster();
-        }
-    }
-
-    public void spawnMonster()
-    {
-        if (monstersWaiting.Count > 0)
+        if (monsterWaiting.Count >= 3)
         {
             GameObject gjm = (GameObject)Instantiate(monster);
             gjm.GetComponent<MonsterMovement>().GameManager = gameManager;
@@ -65,68 +46,59 @@ public class Factory : MonoBehaviour
             gjm.GetComponent<MonsterMovement>().StartMoving();
 
             Monster m = gjm.GetComponent<Monster>();
-            m.movementSpeed += monstersWaiting.Peek()[1];
-            m.maxHealth += monstersWaiting.Peek()[2];
-            m.carryCapacity += monstersWaiting.Peek()[3];
-            m.lineOfSight += monstersWaiting.Peek()[4];
-            m.strength += monstersWaiting.Peek()[5];
 
-            monstersWaiting.Dequeue();
+            foreach (Ressource ressource in monsterWaiting)
+            {
+                switch (ressource)
+                {
+                    case Ressource.FUNGUS:
+                        m.maxHealth += 1;
+                        break;
+
+                    case Ressource.SODA:
+                        m.movementSpeed += 1;
+                        break;
+                }
+            }
+
+            NewMonster();
         }
         
     }
 
+    private void addItem(Ressource ressource)
+    {
+        if (monsterWaiting.Count >= 3) monsterWaiting.Dequeue();
+        monsterWaiting.Enqueue(ressource);
+    }
+
     public void addRecondite()
     {
-        if (recondite > 0)
-        {
-            recondite -= 1;
-            currentRessourceMonster[0] += 1;
-        }
+        addItem(Ressource.RECONDITE);
     }
 
     public void addMovementSpeed()
     {
-        if (movementSpeedRessource > 0)
-        {
-            movementSpeedRessource -= 1;
-            currentRessourceMonster[1] += 1;
-        }
+        addItem(Ressource.SODA);
     }
 
     public void addHitPoints()
     {
-        if (hitPointsRessource > 0)
-        {
-            hitPointsRessource -= 1;
-            currentRessourceMonster[2] += 1;
-        }
+        addItem(Ressource.FUNGUS);
     }
 
     public void addCarryCapacity()
     {
-        if (carryCapacityRessource > 0)
-        {
-            carryCapacityRessource -= 1;
-            currentRessourceMonster[3] += 1;
-        }
+        addItem(Ressource.WEED);
     }
 
     public void addLineOfSight()
     {
-        if (lineOfSightRessource > 0)
-        {
-            lineOfSightRessource -= 1;
-            currentRessourceMonster[4] += 1;
-        }
+        addItem(Ressource.PURPLE_CRISTAL);
     }
 
     public void addStrength()
     {
-        if (strengthRessource > 0)
-        {
-            strengthRessource -= 1;
-            currentRessourceMonster[5] += 1;
-        }
+        addItem(Ressource.MEAT);
     }
 }
