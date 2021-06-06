4d5b9f969414f2fbaf7b2c05c542de54a86834d1
Antoine <antoine35.colin@gmail.com>
2021-06-06 23:30:01 +0200

    Laser visuel pour la tour, à consolider

--- Assets/Scripts/Defense/Tower.cs
+++ Assets/Scripts/Defense/Tower.cs
@@ -13,17 +13,20 @@ public class Tower : MonoBehaviour, ITickable
     public int damage = 3;
     public bool destroyable = false;
     public float radius = 1;
-    
+
     public TowerMode towerMode = TowerMode.SIMPLE;
     public TargetMode targetMode = TargetMode.FIRST;
 
     readonly List<Monster> affectedMonsters = new List<Monster>();
-
+    LineRenderer Laser;
+    Vector3 mobOffset = new Vector3(0, 0.3f, -1);
+    Vector3 towerOffset = new Vector3(0, 1.3f, -1);
 
     public void Start()
     {
         WorldManager.INSTANCE.tick.AddListener(Tick);
         Random.InitState(35354332);
+        Laser = GetComponentInChildren<LineRenderer>();
     }
 
     public void OnDestroy()
@@ -52,7 +55,7 @@ public class Tower : MonoBehaviour, ITickable
     {
         if (affectedMonsters.Count > 0)
         {
-
+            //if (Laser != null) Laser.enabled = false;
             Monster target;
             switch (targetMode)
             {
@@ -84,78 +87,76 @@ public class Tower : MonoBehaviour, ITickable
                     break;
             }
 
+            List<Monster> damagedMonsters = new List<Monster>();
             switch (towerMode)
             {
                 case TowerMode.SIMPLE:
-                    DrawLazer(transform, target.transform);
-                    target.GetHurt(damage);
+                    damagedMonsters.Add(target);
                     break;
 
                 case TowerMode.EXPLODING:
                     RaycastHit2D[] hits;
                     Monster monster;
                     hits = Physics2D.CircleCastAll(target.transform.position, radius, Vector2.right);
-                    foreach(RaycastHit2D hit in hits)
+                    foreach (RaycastHit2D hit in hits)
                     {
                         monster = hit.collider.transform.GetComponent<Monster>();
-                        if(monster != null)
+                        if (monster != null)
                         {
-                            DrawLazer(transform, target.transform);
-                            monster.GetHurt(damage);
+                            damagedMonsters.Add(monster);
                         }
                     }
-
-                    target.GetHurt(damage);
                     break;
 
                 case TowerMode.PERCING:
-                    RaycastHit2D[] hitc; 
+                    RaycastHit2D[] hitc;
                     hitc = Physics2D.LinecastAll(transform.position, target.transform.position);
                     foreach (RaycastHit2D hit in hitc)
                     {
                         monster = hit.collider.transform.GetComponent<Monster>();
                         if (monster != null)
                         {
-                            DrawLazer(transform, target.transform);
-                            monster.GetHurt(damage);
+                            damagedMonsters.Add(monster);
                         }
                     }
-                    target.GetHurt(damage);
-                    DrawLazer(transform, hitc[hitc.Length-1].transform);
-
                     break;
 
                 case TowerMode.CHANNELING:
-                    List<Monster> knownMonsters = new List<Monster>
-                    {
-                        target
-                    };
                     RaycastHit2D[] hitz;
-                    DrawLazer(transform, target.transform);
-                    for (int i = 0; i< knownMonsters.Count; i++)
+                    for (int i = 0; i < damagedMonsters.Count; i++)
                     {
-                        hitz = Physics2D.CircleCastAll(knownMonsters[i].transform.position, radius, Vector2.right);
+                        hitz = Physics2D.CircleCastAll(damagedMonsters[i].transform.position, radius, Vector2.right);
                         foreach (RaycastHit2D hit in hitz)
                         {
                             monster = hit.collider.transform.GetComponent<Monster>();
-                            if (monster != null && !knownMonsters.Contains(monster))
+                            if (monster != null && !damagedMonsters.Contains(monster))
                             {
-                                DrawLazer(knownMonsters[i].transform, monster.transform);
-                                knownMonsters.Add(monster);
-                                monster.GetHurt(damage);
+                                damagedMonsters.Add(monster);
                             }
-                            hit.collider.transform.GetComponent<Monster>()?.GetHurt(damage);
                         }
                     }
-                    target.GetHurt(damage);
-                    
                     break;
             }
+
+            DrawLazer(damagedMonsters.Select(x => x.transform.position + mobOffset).ToList());
+            damagedMonsters.ForEach(x => x.GetHurt(damage));
         }
     }
 
-    public void DrawLazer(Transform from, Transform to)
+    public void DrawLazer(List<Vector3> positions)
     {
-
+        if (Laser != null)
+        {
+            if (positions.Count != 0)
+            {
+                positions.Insert(0, transform.position + towerOffset);
+                Laser.positionCount = positions.Count;
+                Laser.SetPositions(positions.ToArray());
+            }
+            else
+            {
+                Laser.positionCount = 1;
+            }
+        }
     }
 }
